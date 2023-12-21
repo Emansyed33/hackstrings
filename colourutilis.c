5d289ee3d88417a5b89ade4409139167ff7e1376
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-12-21 23:51:22 +0500

    last for ensuring each

--- colourutilis.c
+++ colourutilis.c
@@ -40,3 +40,35 @@ char *removeCharCopy(const char *s, char c) {
     *temp = '\0';
     return newStr;
 }
+char *removeCharCopy(const char *s, char c) {
+    char *newStr = (char *)malloc(strlen(s) + 1); // +1 for null terminator
+    if (!newStr) return NULL;
+
+    char *temp = newStr;
+    while (*s) {
+        if (*s != c) {
+            *temp++ = *s;
+        }
+        s++;
+    }
+    *temp = '\0';
+    return newStr;
+}
+
+char **lengthSplit(const char *s, int n) {
+    int len = strlen(s);
+    int numStrings = (len + n - 1) / n; // Calculate the number of chunks
+    char **splitArray = (char **)malloc(numStrings * sizeof(char *));
+
+    if (!splitArray) return NULL;
+
+    for (int i = 0; i < numStrings; ++i) {
+        splitArray[i] = (char *)malloc(n + 1); // +1 for null terminator
+        if (!splitArray[i]) {
+            // Free previously allocated memory if error occurs
+            while (i--) free(splitArray[i]);
+            free(splitArray);
+            return NULL;
+        }
+        strncpy(splitArray[i], s + i * n, n);
+        splitArray[i][n] = '\0'; // Ensure each
