78e94de384d6e7546eeeb953cee075350c091863
Emansyed33 <144342740+Emansyed33@users.noreply.github.com>
2023-12-21 23:56:19 +0500

    prototypes

---
+++ prototypes.c
@@ -0,0 +1,7 @@
+void replaceChar(char *s, char oldChar, char newChar);
+char *replaceCharCopy(const char *s, char oldChar, char newChar);
+void removeChar(char *s, char c);
+char *removeCharCopy(const char *s, char c);
+char **lengthSplit(const char *s, int n);
+
+#endif // STRING_UTILS_H
